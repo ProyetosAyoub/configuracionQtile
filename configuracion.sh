9552fc1b651950d270d6bf5fd333247847c23d65
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-04-10 04:12:01 +0200

    Update configuracion.sh

--- configuracion.sh
+++ configuracion.sh
@@ -1,70 +1,18 @@
- pacman -S acl 2.3.1- adobe-source-code-pro-fonts 2.040u+1.060i+1.024vf-1 dwaita-cursors 43-2 \
-adwaita-icon-theme 43-2 alacritty 0.12.0-1 alsa-lib 1.2.8-1 alsa-plugins 1:1.2.7.1-2 \
-alsa-topology-conf 1.2.5.1-3 alsa-ucm-conf 1.2.8-1 amdvlk 2022.Q4.4-3 aom 3.6.0-1 \
-arandr 0.1.11-1 archlinux-keyring 20230320-1 argon2 20190702-4 at-spi2-core 2.48.0-1 \
-atkmm 2.28.3-1 attr 2.5.1-3 audit 3.1-1 autoconf 2.71-4 automake 1.16.5-2 avahi 0.8+22+gfd482a7-3 \
-awesome-terminal-fonts 1.1.0-4 base 3-1 base-devel 1-1 bash 5.1.016-3 bat 0.23.0-3 binutils 2.40-4 \ 
-bison 3.8.2-5 bluez-libs 5.66-1 brotli 1.0.9-10 btrfs-progs 6.2.2-1 bzip2 1.0.8-5 c-ares 1.19.0-1 \
-ca-certificates 20220905-1 ca-certificates-mozilla 3.89-1 ca-certificates-utils 20220905-1 cairo 1.17.8-2 \
-cairomm 1.14.4-1 cantarell-fonts 1:0.303.1-1 code 1.77.1-1 coreutils 9.2-3 cryptsetup 2.6.1-3 curl 8.0.1-1 \ 
-dav1d 1.1.0-1 db5.3 5.3.28-2 dbus 1.14.6-2 dbus-glib 0.112-2 dconf 0.40.0-2 debugedit 5.0-5 default-cursors 2-1 \
-desktop-file-utils 0.26-2 device-mapper 2.03.20-1 dhcpcd 9.4.1-1 diffutils 3.9-1 discord 0.0.26-1 dkms 3.0.10-1 \
-dmraid 1.0.0.rc16.3-14 dnssec-anchors 20190629-3 dosfstools 4.2-3 double-conversion 3.2.1-1 duktape 2.7.0-5 \
-e2fsprogs 1.47.0-1 electron19 19.1.9-5 engrampa 1.26.0-1 expat 2.5.0-1 fakeroot 1.31-2 ffmpeg 2:6.0-4 fftw 3.3.10-4 \
-file 5.44-3 filesystem 2023.01.31-1 findutils 4.9.0-3 firefox 111.0.1-1 flac 1.4.2-1 flex 2.6.4-5 fontconfig 2:2.14.2-1 freetype2 2.13.0-1 \
-fribidi 1.0.12-1 fuse-common 3.14.1-1 fuse2 2.9.9-4 gawk 5.2.1-2 gc 8.2.2-1 gcc 12.2.1-2 gcc-libs 12.2.1-2 gcr-4 4.0.0-1 gdbm 1.23-2 gdk-pixbuf2 2.42.10-2 \
-gettext 0.21.1-5 giflib 5.2.1-2 git 2.40.0-1 glib-networking 1:2.76.0-1 glib2 2.76.1-1 glibc 2.37-2 glibmm 2.66.6-1 glu 9.0.2-3 \
-gmp 6.2.1-2 gnu-free-fonts 20120503-8 gnupg 2.2.41-1 gnutls 3.8.0-1 go 2:1.20.3-1 gobject-introspection-runtime 1.76.1-1 google-chrome 112.0.5615.49-1 \
-gperftools 2.10-1 gpgme 1.19.0-3 gpm 1.20.7.r38.ge82d1a6-4 gptfdisk 1.0.9-2 graphite 1:1.3.14-3 grep 3.10-1 groff 1.22.4-10 grub 2:2.06.r456.g65bc45963-1 \
-gsettings-desktop-schemas 44.0-1 gsm 1.0.22-1 gtk-update-icon-cache 1:4.10.1-2 gtk2 2.24.33-2 gtk3 1:3.24.37-1 gtkmm 1:2.24.5-4 gtkmm3 3.24.7-1 \
-guile 3.0.9-1 gzip 1.12-2 harfbuzz 7.1.0-1 hicolor-icon-theme 0.17-3 hidapi 0.13.1-2 highway 1.0.4-1 http-parser 2.9.4-1 hwdata 0.369-1 \
-hwloc 2.9.0-1 iana-etc 20230306-1 icu 72.1-2 imath 3.1.7-1 iniparser 4.1-4 iproute2 6.2.0-2 iptables 1:1.8.9-1 iputils 20221126-1 iso-codes 4.13.0-1 \
-jack2 1.9.22-1 jansson 2.14-2 json-c 0.16-1 json-glib 1.6.6-2 kbd 2.5.1-1 keyutils 1.6.3-1 kmod 30-3 krb5 1.20.1-1 l-smash 2.14.5-3 lame 3.100-4 \
-lcms2 2.15-1 ldns 1.8.3-2 less 1:608-2 lib32-alsa-lib 1.2.8-1 lib32-alsa-plugins 1.2.7.1-2 lib32-amdvlk 2022.Q4.4-3 lib32-brotli 1.0.9-5 lib32-bzip2 1.0.8-3 \
-lib32-curl 8.0.1-1 lib32-e2fsprogs 1.47.0-1 lib32-expat 2.5.0-2 lib32-gcc-libs 12.2.1-2 lib32-glibc 2.37-2 lib32-icu 72.1-2 lib32-keyutils 1.6.3-1 lib32-krb5 1.20.1-1 \
-lib32-libdrm 2.4.115-1 lib32-libelf 0.189-1 lib32-libffi 3.4.4-1 lib32-libglvnd 1.6.0-1 lib32-libgpg-error 1.46-1 lib32-libidn2 2.3.4-2 lib32-libldap 2.6.4-1 \
-lib32-libpciaccess 0.17-1 lib32-libpsl 0.21.2-1 lib32-libssh2 1.10.0-2 lib32-libtasn1 4.19.0-1 lib32-libunistring 1.1-1 lib32-libunwind 1.6.2-2 lib32-libx11 1.8.4-1 \
-lib32-libxau 1.0.11-1 lib32-libxcb 1.15-2 lib32-libxcrypt 4.4.33-1 lib32-libxdamage 1.1.6-1 lib32-libxdmcp 1.1.4-1 lib32-libxext 1.3.5-1 lib32-libxfixes 6.0.0-2 \
-lib32-libxml2 2.10.3-2 lib32-libxshmfence 1.3.2-1 lib32-libxss 1.2.3-3 lib32-libxxf86vm 1.1.5-1 lib32-llvm-libs 15.0.7-1 lib32-lm_sensors 1:3.6.0.r41.g31d1f125-2 \
-lib32-mesa 23.0.1-2 lib32-ncurses 6.4-1 lib32-nspr 4.35-1 lib32-nss 3.89-1 lib32-openssl 1:3.0.8-1 lib32-p11-kit 0.24.1-1 lib32-sqlite 3.41.1-1 lib32-vulkan-icd-loader 1.3.245-1 \
-lib32-wayland 1.21.0-1 lib32-xz 5.4.2-1 lib32-zlib 1.2.13-2 lib32-zstd 1.5.4-1 libaio 0.3.113-2 libappindicator-gtk3 12.10.0.r298-1 libarchive 3.6.2-2 libass 0.17.1-1 \
-libassuan 2.5.5-2 libasyncns 1:0.8+r3+g68cd5af-2 libatasmart 0.19-5 libavc1394 0.5.4-5 libblockdev 2.28-1 libbluray 1.3.4-1 libbpf 1.1.0-1 libbs2b 3.1.0-8 libbytesize 2.7-1 \
-libcanberra 1:0.30+r2+gc0620e4-3 libcap 2.68-1 libcap-ng 0.8.3-1 libcloudproviders 0.3.1+r8+g3a229ee-1 libcolord 1.4.6-1 libconfig 1.7.3-2 libcups 1:2.4.2-6 \
-libdaemon 0.14-5 libdatrie 0.2.13-2 libdbusmenu-glib 16.04.0-5 libdbusmenu-gtk3 16.04.0-5 libdrm 2.4.115-1 libedit 20221030_3.1-1 libelf 0.189-1 libepoxy 1.5.10-1 \
-libev 4.33-2 libevdev 1.13.0-1 libevent 2.1.12-4 libexif 0.6.24-1 libffi 3.4.4-1 libfm 1.3.2-1 libfm-extra 1.3.2-1 libfm-gtk2 1.3.2-1 libfontenc 1.1.7-1 \
-libgcrypt 1.10.1-2 libgirepository 1.76.1-1 libgit2 1:1.6.3-1 libglvnd 1.6.0-1 libgpg-error 1.46-2 libgudev 237-2 libice 1.1.1-2 libidn2 2.3.4-3 libiec61883 1.2.0-7 \
-libindicator-gtk3 12.10.1-10 libinih 56-1 libinput 1.23.0-1 libisl 0.25-1 libjpeg-turbo 2.1.5.1-1 libjxl 0.8.1-1 libksba 1.6.3-1 libldap 2.6.4-1 liblzf 3.6-4 \
-libmfx 23.1.3-2 libmm-glib 1.20.6-1 libmnl 1.0.5-1 libmodplug 0.8.9.0-5 libmpc 1.3.1-1 libndp 1.8-1 libnetfilter_conntrack 1.0.9-1 libnewt 0.52.23-1 libnfnetlink 1.0.2-1 \
-libnftnl 1.2.5-1 libnghttp2 1.52.0-2 libnl 3.7.0-3 libnm 1.42.4-2 libnma 1.10.6-1 libnma-common 1.10.6-1 libnotify 0.8.2-1 libnsl 2.0.0-3 libogg 1.3.5-1 libomxil-bellagio 0.9.3-4 \
-libopenmpt 0.6.9-2 libp11-kit 0.24.1-1 libpcap 1.10.3-1 libpciaccess 0.17-1 libpgm 5.3.128-2 libpng 1.6.39-1 libproxy 0.4.18-2 libpsl 0.21.2-1 libpulse 16.1-6 \
-libraw1394 2.1.2-3 librsvg 2:2.56.0-1 libsamplerate 0.2.2-2 libsasl 2.1.28-4 libseccomp 2.5.4-1 libsecret 0.20.5-2 libsigc++ 2.12.0-1 libsm 1.2.4-1 libsndfile 1.2.0-1 \
-libsodium 1.0.18-2 libsoup3 3.4.0-1 libsoxr 0.1.3-3 libssh 0.10.4-3 libssh2 1.10.0-3 libstemmer 2.2.0-2 libsysprof-capture 3.48.0-1 libtasn1 4.19.0-1 libteam 1.31-7 \
-libthai 0.1.29-2 libtheora 1.1.1-5 libtiff 4.5.0-3 libtirpc 1.3.3-2 libtool 2.4.7+4+g1ec8fa28-2 libtpms 0.9.5-2 libtraceevent 1:1.7.2-1 libtracefs 1.6.4-2 \
-libunistring 1.1-2 libunwind 1.6.2-2 liburcu 0.14.0-1 libusb 1.0.26-2 libutempter 1.2.1-3 libva 2.18.0-1 libvdpau 1.5-1 libverto 0.3.2-4 libvorbis 1.3.7-3 \
-libvpx 1.13.0-1 libwacom 2.6.0-2 libwebp 1.3.0-2 libx11 1.8.4-1 libxau 1.0.11-2 libxaw 1.0.15-1 libxcb 1.15-2 libxcomposite 0.4.6-1 libxcrypt 4.4.33-1 libxcursor 1.2.1-3 \
-libxcvt 0.1.2-1 libxdamage 1.1.6-1 libxdg-basedir 1.2.3-1 libxdmcp 1.1.4-2 libxext 1.3.5-1 libxfixes 6.0.0-2 libxfont2 2.0.6-2 libxft 2.3.7-1 libxi 1.8-3 \
-libxinerama 1.1.5-1 libxkbcommon 1.5.0-1 libxkbcommon-x11 1.5.0-1 libxkbfile 1.1.2-1 libxklavier 5.4-4 libxml2 2.10.3-2 libxmu 1.1.4-1 libxpm 3.5.15-1 libxrandr 1.5.3-1 \
-libxrender 0.9.11-1 libxshmfence 1.3.2-1 libxslt 1.1.37-2 libxss 1.2.3-4 libxt 1.2.1-1 libxtst 1.2.4-1 libxv 1.0.12-1 libxxf86vm 1.1.5-1 libyaml 0.2.5-2 licenses 20220125-2 \
-lightdm 1:1.32.0-4 lightdm-gtk-greeter 1:2.0.8-3 linux 6.2.9.arch1-1 linux-api-headers 6.1.9-1 linux-firmware 20230210.bf4115c-1 linux-firmware-whence 20230210.bf4115c-1 \
-llvm-libs 15.0.7-2 lm_sensors 1:3.6.0.r41.g31d1f125-2 lsb-release 2.0.r48.3cf5103-1 lsd 0.23.1-1 lsof 4.98.0-1 luit 20221028-1 lvm2 2.03.20-1 lxappearance 0.6.3-4 \
-lxmenu-data 0.1.5-3 lz4 1:1.9.4-1 lzo 2.10-5 m4 1.4.19-3 mailcap 2.1.53-1 make 4.4.1-2 md4c 0.4.8-1 mdadm 4.2-2 menu-cache 1.1.0-4 mesa 23.0.1-2 minizip 1:1.2.13-2 \
-mkinitcpio 35.2-1 mkinitcpio-busybox 1.35.0-1 mobile-broadband-provider-info 20221107-2 mpfr 4.2.0-3 mpg123 1.31.3-1 mtdev 1.1.6-2 nano 7.2-1 ncurses 6.4-1 ndctl 76.1-1 \
-neofetch 7.1.0-2 nettle 3.8.1-1 network-manager-applet 1.30.0-1 networkmanager 1.42.4-2 nitrogen 1.6.1-4 nm-connection-editor 1.30.0-1 noto-fonts 20230401-1 \
-npth 1.6-4 nspr 4.35-1 nss 3.89-1 ntfs-3g 2022.10.3-1 ocl-icd 2.3.1-1 oniguruma 6.9.8-1 opencore-amr 0.1.6-1 openexr 3.1.7-1 openjpeg2 2.5.0-2 openmpi 4.1.5-1 \
-openpmix 4.2.3-2 openssh 9.3p1-1 openssl 3.0.8-1 opus 1.3.1-3 orc 0.4.33-1 p11-kit 0.24.1-1 pacman 6.0.2-6 pacman-mirrorlist 20230226-1 pam 1.5.2-1 pambase 20221020-1 \
-pamixer 1.6-2 pango 1:1.50.14-1 pangomm 2.46.3-1 parted 3.5-1 patch 2.7.6-9 pavucontrol 1:5.0+r61+gee77d86-2 pciutils 3.9.0-2 pcmanfm 1.3.2-1 pcre 8.45-3 pcre2 10.42-2 \
-pcsclite 1.9.9-3 perl 5.36.0-3 perl-error 0.17029-4 perl-mailtools 2.21-6 perl-timedate 2.33-4 picom 10.2-1 pinentry 1.2.1-1 pixman 0.42.2-1 pkgconf 1.8.1-1 \
-polkit 122-1 popt 1.19-1 portaudio 1:19.7.0-2 procps-ng 3.3.17-1 psmisc 23.6-1 pulseaudio 16.1-6 python 3.10.10-1 python-cairo 1.23.0-3 python-cairocffi 1.4.0-1 \
-python-cffi 1.15.1-1 python-docopt 0.6.2-11 python-gobject 3.44.1-1 python-keyutils 0.6-8 python-ply 3.11-10 python-pycparser 2.21-3 python-xcffib 1.2.0-1 \
-python-yaml 6.0-1 qt5-base 5.15.8+kde+r185-1 qt5-tools 5.15.8+kde+r1-3 qt5-translations 5.15.8-1 qt5-x11extras 5.15.8-1 qtile 0.22.1-3 rav1e 0.6.3-1 \
-re2 1:20230301-1 readline 8.2.001-2 ripgrep 13.0.0-3 rofi 1.7.5-1 rtkit 0.13-2 sdl12-compat 1.2.60-1 sdl2 2.26.4-1 seatd 0.7.0-3 sed 4.9-3 shadow 4.13-2 \
-shared-mime-info 2.2+13+ga2ffb28-1 slang 2.3.3-2 snappy 1.1.9-2 sound-theme-freedesktop 0.8-5 speex 1.2.1-1 speexdsp 1.2.1-1 sqlite 3.41.2-1 srt 1.5.1-3 \
-startup-notification 0.12-7 steam 1.0.0.76-1 sudo 1.9.13.p3-1 svt-av1 1.4.1-1 sysfsutils 2.1.1-1 systemd 253.3-1 systemd-libs 253.3-1 systemd-sysvcompat 253.3-1 \
-tar 1.34-2 tdb 1.4.8-1 texinfo 7.0.3-1 thin-provisioning-tools 1.0.3-1 tpm2-tss 3.2.0-3 tracker3 3.5.0-1 tslib 1.22-1 ttf-dejavu 2.37+18+g9b5d1b2f-3 ttf-liberation 2.1.5-1 \
-tzdata 2023c-1 udiskie 2.4.2-1 udisks2 2.9.4-1 unzip 6.0-19 usbutils 015-2 util-linux 2.38.1-3 util-linux-libs 2.38.1-3 v4l-utils 1.24.1-1 vid.stab 1.1-3 \
-virtualbox 7.0.6-3 virtualbox-host-dkms 7.0.6-3 vmaf 2.3.1-1 volume_key 0.3.12-7 vulkan-icd-loader 1.3.245-1 wayland 1.22.0-1 webrtc-audio-processing 0.3.1-3 \
-which 2.21-6 wlroots0.15 0.15.1-1 wpa_supplicant 2:2.10-8 x264 3:0.164.r3095.baee400-4 x265 3.5-3 xbitmaps 1.1.3-1 xcb-proto 1.15.2-2 xcb-util 0.4.1-1 xcb-util-cursor 0.1.4-1 \
-xcb-util-errors 1.0.1-1 xcb-util-image 0.4.1-2 xcb-util-keysyms 0.4.1-4 xcb-util-renderutil 0.3.10-1 xcb-util-wm 0.4.2-1 xcb-util-xrm 1.3-2 xdg-utils 1.1.3+25+g8ae0263-1 
-xf86-input-libinput 1.3.0-1 xfsprogs 6.2.0-1 xkeyboard-config 2.38-1 xorg-fonts-encodings 1.0.7-1 xorg-server 21.1.8-1 xorg-server-common 21.1.8-1 xorg-setxkbmap 1.3.3-2 \
-xorg-xkbcomp 1.4.6-1 xorg-xmodmap 1.0.11-1 xorg-xprop 1.2.6-1 xorg-xrandr 1.5.2-1 xorg-xrdb 1.2.1-1 xorg-xset 1.2.5-1 xorgproto 2022.2-1 xterm 379-1 xvidcore 1.3.7-2 \
-xz 5.4.2-1 yay 12.0.3-1 zenity 3.44.0-3 zeromq 4.3.4-2 zimg 3.0.4-1 zip 3.0-10 zlib 1:1.2.13-2  zstd 1.5.4-1
+sudo pacman -S code arandr nitrogen lxappearance firefox engrampa picom lsd curl virtualbox discord
+
+sudo usermod -aG vboxusers $USER
+
+git clone --depth=1 https://github.com/adis1090x/rofi.git
+
+cd rodi
+chmod +x setup.sh
+
+./setup.sh
+
+sudo pacman -S rofi
+sudo pacman -S bat
+sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+nano ~/.bashrc < EOF
+echo "alias ls="lsd"",
+echo "alias cat="bat""
